b90e698d7d79a1158e8a40e9712b6be8f767b243
Tems <tems@tems.pl>
2024-02-08 16:44:17 +0100

    Top players & whole earnings chart

--- src/components/stats/Stats.js
+++ src/components/stats/Stats.js
@@ -1,6 +1,8 @@
 import React from "react";
 import GeneralStats from "./GeneralStats";
 import ProductsBought from "./ProductsBought";
+import WholeEarningsChart from "../charts/WholeEarningsChart";
+import TopPlayers from "../charts/TopPlayers";
 
 const Stats = props => {
     const { data } = props;
@@ -8,6 +10,8 @@ const Stats = props => {
     return (
         <div className="w-[95%] h-[95%] bg-slate-500 p-3 rounded-md mb-5 flex flex-col gap-3">
             <GeneralStats data={data} products={props.products} />
+            <WholeEarningsChart data={data} products={props.products} />
+            <TopPlayers data={data} products={props.products} />
             <ProductsBought data={data} />
         </div>
     );
